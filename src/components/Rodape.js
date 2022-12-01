a12157d60379285b8dac92711d6fa03d96c3ac0e
Laura <laurasouzacastro1@gmail.com>
2022-12-01 18:31:36 -0300

    correção de tag para correção automática

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -7,7 +7,7 @@ import sad from "../assets/sad.png";
 export default function Rodape({cards, cardsFeitos, respostas}){
     return(
         <FooterConcluidos data-test="footer">
-            <Mensagem data-test="finish-text">{(cardsFeitos.length === cards.length) ? <><span><img src={respostas.find(e => e.icone === erroIcone) ? sad : party} alt="" />{respostas.find(e => e.icone === erroIcone) ? 'Putz...' : 'Parabéns!'}</span><p>{respostas.find(e => e.icone === erroIcone) ? <>Ainda faltam alguns... <br/> Mas não desanime!</> : 'Você não esqueceu de nenhum flashcard!'}</p></> : ""}</Mensagem>
+            <Mensagem data-test="finish-text">{(cardsFeitos.length === cards.length) ? <><span><img src={respostas.find(e => e.icone === erroIcone) ? sad : party} alt="" />{respostas.find(e => e.icone === erroIcone) ? 'Putz...' : 'Parabéns!'}</span><p>{respostas.find(e => e.icone === erroIcone) ? <>Ainda faltam alguns... <br/> Mas não desanime!</> : 'Você não esqueceu de nenhum flashcard!'}</p></> : <div></div>}</Mensagem>
             {cardsFeitos.length}/{cards.length} CONCLUÍDOS
             <ContainerBotoes>
                 {respostas.map((r) => <img key={r.id} src={r.icone} alt="" data-test={r.icone === erroIcone ? "no-icon" : r.icone === certoIcone ? "zap-icon": r.icone === quaseIcone ? "partial-icon" : ""}/>)}
